a09abe4697e051e34dbaa7d1a17d14c7b284902a
GGaboBD <007007gabo@gmail.com>
2026-06-08 15:50:38 -0600

    Segundo commit de la guia: Implementado el método enganchar_vagon con validaciones de peso y cisterna

--- PartePractica_1.py
+++ PartePractica_1.py
@@ -37,16 +37,41 @@ class Tren:
         self.locomotora = locomotora
         self._vagones = []
     
-    def enganchar_vagon(self, vagon: Vagon):
-        nuevo_peso = self.obtener_peso_tren() + vagon.calcular_peso_total()
-        
-        if nuevo_peso > self.locomotora.capacidad_arrastre:
-            raise ValueError("Capacidad de arrastre excedida")
-        else:
-            self._vagones.append(vagon)
 
     def obtener_peso_tren(self):
         total = 0
         for vagon in self._vagones: 
             total += vagon.calcular_peso_total()
         return total
+    
+    def enganchar_vagon(self, vagon: Vagon):
+        nuevo_peso_tren = self.obtener_peso_tren() + vagon.calcular_peso_total()
+
+        if nuevo_peso_tren > self.locomotora.capacidad_arrastre:
+            raise ValueError("Capacidad de arrastre excedida")
+
+        conteo_cisternas = 0
+        for v in self._vagones:
+            if isinstance(v, VagonCisterna):
+                conteo_cisternas += 1
+
+        if isinstance(vagon, VagonCisterna):
+            conteo_cisternas+=1
+
+        peso_futuro_cisternas = 0
+        for v in self._vagones:
+            if isinstance(v, VagonCisterna):
+                peso_futuro_cisternas += v.calcular_peso_total()
+        
+        if isinstance(vagon, VagonCisterna):
+            peso_futuro_cisternas += vagon.calcular_peso_total()
+
+        if conteo_cisternas > 3:
+            raise ValueError("Restricción de seguridad de Vagones Cisterna violada")
+        
+        if nuevo_peso_tren > 0: # Evitar división por cero
+            porcentaje_cisternas = peso_futuro_cisternas / nuevo_peso_tren
+            if porcentaje_cisternas > 0.60:
+                raise ValueError("Restricción de seguridad de Vagones Cisterna violada")
+            
+        self._vagones.append(vagon)
